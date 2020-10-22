4888e37087b38388a8b82fe0b3a3bb9515b16fd1
ktv-sky <helicooper7@gmail.com>
2020-10-22 14:22:15 +0300

    Add base template, Image validation form

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,10 +1,30 @@
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
 
 from .models import Cart, CartProduct, Category, Customer, Notebook, Smartphone
+from PIL import Image
+
+
+class NoteBookAdminForm(ModelForm):
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загружайте изображения с минимальным разрешением  {}x{}'.format(
+            *self.MIN_RESOLUTION
+            )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального')
+        return image
 
 
 class NotebookAdmin(admin.ModelAdmin):
+    form = NoteBookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    return render(request, 'base.html')
