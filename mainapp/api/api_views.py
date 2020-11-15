5bfa0cdb5b26ff2b0d1721bfb3442bf53f7f47ed
ktv-sky <helicooper7@gmail.com>
2020-11-15 19:31:08 +0300

    Add pagination

--- mainapp/api/api_views.py
+++ mainapp/api/api_views.py
@@ -1,15 +1,23 @@
-from django.db.models import query
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.pagination import PageNumberPagination
 
-from ..models import Category, Notebook, Smartphone, Customer
-from .serializers import (CategorySerializer, NotebookSerializer,
-                          SmartphoneSerializer, CustomerSerializer)
+from ..models import Category, Customer, Notebook, Smartphone
+from .serializers import (CategorySerializer, CustomerSerializer,
+                          NotebookSerializer, SmartphoneSerializer)
+
+
+class CategoryPagination(PageNumberPagination):
+
+    page_size = 2
+    page_query_param = 'page_size'
+    max_page_size = 10
 
 
 class CategoryListAPIView(ListAPIView):
 
     serializer_class = CategorySerializer
+    pagination_class = CategoryPagination
     queryset = Category.objects.all()
 
 
