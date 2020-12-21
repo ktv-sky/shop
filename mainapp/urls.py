0d2e81cc1f12bd64b0c13b438ee3685766159ab7
KTV <ktv@MacBook-Pro-KTV.local>
2020-12-21 15:57:28 +0300

    Edit visual code

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,23 +1,24 @@
-from django.urls import path
 from django.contrib.auth.views import LogoutView
+from django.urls import path
 
 from .views import (AddToCartView, BaseView, CartView, CategoryDetailView,
                     ChangeQTYView, CheckoutView, DeleteFromCartView, LoginView,
-                    MakeOrderView, ProductDetailView, ProfileView, RegistrationView)
+                    MakeOrderView, ProductDetailView, ProfileView,
+                    RegistrationView)
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
-    path('products/<str:slug>/',
-         ProductDetailView.as_view(), name='product_detail'),
+    path('products/<str:slug>/', ProductDetailView.as_view(),
+        name='product_detail'),
     path('category/<str:slug>/', CategoryDetailView.as_view(),
-         name='category_detail'),
+        name='category_detail'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('add-to-cart/<str:slug>/',
-         AddToCartView.as_view(), name='add_to_cart'),
-    path('remove-from-cart/<str:slug>/',
-         DeleteFromCartView.as_view(), name='delete_from_cart'),
-    path('change-qty/<str:slug>/',
-         ChangeQTYView.as_view(), name='change_qty'),
+    path('add-to-cart/<str:slug>/', AddToCartView.as_view(),
+        name='add_to_cart'),
+    path('remove-from-cart/<str:slug>/', DeleteFromCartView.as_view(),
+        name='delete_from_cart'),
+    path('change-qty/<str:slug>/', ChangeQTYView.as_view(),
+        name='change_qty'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('make-order/', MakeOrderView.as_view(), name='make_order'),
     path('login/', LoginView.as_view(), name='login'),
