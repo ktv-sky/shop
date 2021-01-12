76474b5e526fe766202670dab308b6d533728ab0
KTV <helicooper7@gmail.com>
2021-01-12 12:06:49 +0300

    Edit postgresql settings

--- shop/settings.py
+++ shop/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ecom_db',
-        'USER': 'ecomuser',
+        'USER': 'ecom_user',
         'PASSWORD': 'devpass',
         'HOST':'127.0.0.1',
         'PORT': '5432'
